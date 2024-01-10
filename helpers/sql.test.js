14ead84a6542bca9b8b3117cdc8195351346a3de
Matt Smida <mcs@m4t7.com>
2024-01-10 14:51:19 -0500

    added tests for sqlForPartialUpdate

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,10 +1,11 @@
 "use strict";
 
 const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require('../expressError');
 
 describe(" ", function () {
 
-  test(" ", function () {
+  test("whether js names translate to sql names for valid inputs", function () {
     const dataToUpdate = {
       testValue: "test value",
       anotherTestValue: "another test value"
@@ -18,6 +19,29 @@ describe(" ", function () {
 
     expect(result).toEqual({
       setCols: `"${jsToSql.testValue}"=$1, "${jsToSql.anotherTestValue}"=$2`,
+      values: [dataToUpdate.testValue, dataToUpdate.anotherTestValue]
+    });
+  });
+
+  test("whether providing no keys throws an error", function () {
+    const dataToUpdate = {};
+    const jsToSql = {};
+
+    expect(() => sqlForPartialUpdate(dataToUpdate, jsToSql))
+     .toThrow(new BadRequestError('No data'));
+  });
+
+  test("whether names col is using idx if name is not in jsToSql", function () {
+    const dataToUpdate = {
+      testValue: "test value",
+      anotherTestValue: "another test value"
+    };
+    const jsToSql = {};
+
+    const result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+
+    expect(result).toEqual({
+      setCols: `"testValue"=$1, "anotherTestValue"=$2`,
       values: [dataToUpdate.testValue, dataToUpdate.anotherTestValue]
     });
   });
