5eea624bb64a1894a011e28e2d05c3d62ebd7898
Matt Smida <mcs@m4t7.com>
2024-01-12 14:35:10 -0500

    added files for jobs routes

---
+++ routes/jobs.js
@@ -0,0 +1,134 @@
+"use strict";
+
+/** Routes for companies. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureAdminLoggedIn } = require("../middleware/auth");
+const Company = require("../models/company");
+
+const companyNewSchema = require("../schemas/companyNew.json");
+const companyUpdateSchema = require("../schemas/companyUpdate.json");
+
+const router = new express.Router();
+
+
+/** POST / { company } =>  { company }
+ *
+ * company should be { handle, name, description, numEmployees, logoUrl }
+ *
+ * Returns { handle, name, description, numEmployees, logoUrl }
+ *
+ * Authorization required: login
+ */
+
+router.post("/", ensureAdminLoggedIn, async function (req, res, next) {
+  const validator = jsonschema.validate(
+    req.body,
+    companyNewSchema,
+    { required: true }
+  );
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const company = await Company.create(req.body);
+  return res.status(201).json({ company });
+});
+
+/** GET /  =>
+ *   { companies: [ { handle, name, description, numEmployees, logoUrl }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - minEmployees
+ * - maxEmployees
+ * - nameLike (will find case-insensitive, partial matches)
+ *
+ * Authorization required: none
+ */
+
+router.get("/", async function (req, res, next) {
+  console.log('running get companies, query:', req.query);
+
+  const queryParams = Object.keys(req.query);
+
+  if (queryParams.length > 0) {
+    const allowedFilters = ['nameLike', 'minEmployees', 'maxEmployees'];
+    const nonallowedFilters = queryParams.filter(
+      param => !allowedFilters.includes(param)
+    );
+
+    if (nonallowedFilters.length > 0) {
+      throw new BadRequestError(
+        "Allowed search fields: nameLike, minEmployees, maxEmployees"
+      );
+    }
+
+    if ('minEmployees' in req.query && 'maxEmployees' in req.query
+      && req.query.minEmployees > req.query.maxEmployees) {
+
+      throw new BadRequestError(
+        "minEmployees cannot be greater than maxEmployees"
+      );
+    }
+  }
+
+  const companies = await Company.findAll(req.query);
+
+  return res.json({ companies });
+});
+
+/** GET /[handle]  =>  { company }
+ *
+ *  Company is { handle, name, description, numEmployees, logoUrl, jobs }
+ *   where jobs is [{ id, title, salary, equity }, ...]
+ *
+ * Authorization required: none
+ */
+
+router.get("/:handle", async function (req, res, next) {
+  const company = await Company.get(req.params.handle);
+  return res.json({ company });
+});
+
+/** PATCH /[handle] { fld1, fld2, ... } => { company }
+ *
+ * Patches company data.
+ *
+ * fields can be: { name, description, numEmployees, logo_url }
+ *
+ * Returns { handle, name, description, numEmployees, logo_url }
+ *
+ * Authorization required: login
+ */
+
+router.patch("/:handle", ensureAdminLoggedIn, async function (req, res, next) {
+  const validator = jsonschema.validate(
+    req.body,
+    companyUpdateSchema,
+    { required: true }
+  );
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const company = await Company.update(req.params.handle, req.body);
+  return res.json({ company });
+});
+
+/** DELETE /[handle]  =>  { deleted: handle }
+ *
+ * Authorization: login
+ */
+
+router.delete("/:handle", ensureAdminLoggedIn, async function (req, res, next) {
+  await Company.remove(req.params.handle);
+  return res.json({ deleted: req.params.handle });
+});
+
+
+module.exports = router;
